42c06b14d6e212b1b18c33dee37a80b13dd763e7
Allan <allan.tuikong@student.moringaschool.com>
2022-08-19 17:24:41 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+// has a function expression called divide
+
+let divide = function (div1, div2) {
+    return 2000/100
+};
+
+// has an arrow function called square
+
+let square = x => x ** 2;
+
+// has an arrow function called add
+
+let add = (x, y) => x + y;
+
